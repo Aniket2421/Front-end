e744e9290455032a65d2c83e391fb8f7c06bdcbe
Aniket2421 <aniketnerkar123@gmail.com>
2023-09-03 22:32:51 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup'
+import Login from './pages/Login';
+import { ToastContainer } from "react-toastify";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const userData = localStorage.getItem('users')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            {
+              userData ?
+                <Route path='/' element={<Dashboard />} />
+                :
+                <Route path='/' element={<Signup />} />
+            }
+          </Route>
+          <Route path='/user/dashboard' element={<Dashboard />} />
+          <Route path='/user/signup' element={<Signup />} />
+          <Route path='/user/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
